3b82acc449c0fb5807f43f62c6f63644069dc413
KamradSmeshnyavy <ibragimovdenis36@gmail.com>
2026-01-28 04:25:27 +0300

    convenient keymaps

--- nvim/lua/plugins/multicursor.nvim.lua
+++ nvim/lua/plugins/multicursor.nvim.lua
@@ -1,61 +1,76 @@
 return {
-    "jake-stewart/multicursor.nvim",
-    branch = "1.0",
-    config = function()
-        local mc = require("multicursor-nvim")
-        mc.setup()
+  "jake-stewart/multicursor.nvim",
+  branch = "1.0",
+  config = function()
+    local mc = require("multicursor-nvim")
+    mc.setup()
 
-        local set = vim.keymap.set
+    local set = vim.keymap.set
 
-        -- Add or skip cursor above/below the main cursor.
-        set({"n", "x"}, "<C-A-up>", function() mc.lineAddCursor(-1) end)
-        set({"n", "x"}, "<C-A-down>", function() mc.lineAddCursor(1) end)
-        set({"n", "x"}, "<leader><C-A-up>", function() mc.lineSkipCursor(-1) end)
-        set({"n", "x"}, "<leader><C-A-down>", function() mc.lineSkipCursor(1) end)
+    -- Add or skip cursor above/below the main cursor.
+    set({ "n", "x" }, "<C-A-up>", function()
+      mc.lineAddCursor(-1)
+    end)
+    set({ "n", "x" }, "<C-A-down>", function()
+      mc.lineAddCursor(1)
+    end)
+    set({ "n", "x" }, "<S-C-A-up>", function()
+      mc.lineSkipCursor(-1)
+    end)
+    set({ "n", "x" }, "<S-C-A-down>", function()
+      mc.lineSkipCursor(1)
+    end)
 
-        -- Add or skip adding a new cursor by matching word/selection
-        set({"n", "x"}, "<leader><C-n>", function() mc.matchAddCursor(1) end)
-        set({"n", "x"}, "<leader><C-s>", function() mc.matchSkipCursor(1) end)
-        set({"n", "x"}, "<leader><C-N>", function() mc.matchAddCursor(-1) end)
-        set({"n", "x"}, "<leader><C-S>", function() mc.matchSkipCursor(-1) end)
+    -- Add or skip adding a new cursor by matching word/selection
+    set({ "n", "x" }, "<leader><C-C>n", function()
+      mc.matchAddCursor(1)
+    end, { desc = "Add  adding a new cursor by matching word/selection 1" })
+    set({ "n", "x" }, "<leader><C-C>s", function()
+      mc.matchSkipCursor(1)
+    end, { desc = "skip adding a new cursor by matching word/selection 1" })
+    set({ "n", "x" }, "<leader><C-C>N", function()
+      mc.matchAddCursor(-1)
+    end, { desc = "Add adding a new cursor by matching word/selection -1" })
+    set({ "n", "x" }, "<leader><C-C>S", function()
+      mc.matchSkipCursor(-1)
+    end, { desc = "skip adding a new cursor by matching word/selection -1" })
 
-        -- Add and remove cursors with control + left click.
-        set("n", "<c-leftmouse>", mc.handleMouse)
-        set("n", "<c-leftdrag>", mc.handleMouseDrag)
-        set("n", "<c-leftrelease>", mc.handleMouseRelease)
+    -- Add and remove cursors with control + left click.
+    set("n", "<c-leftmouse>", mc.handleMouse)
+    set("n", "<c-leftdrag>", mc.handleMouseDrag)
+    set("n", "<c-leftrelease>", mc.handleMouseRelease)
 
-        -- Disable and enable cursors.
-        set({"n", "x"}, "<c-q>", mc.toggleCursor)
+    -- Disable and enable cursors.
+    set({ "n", "x" }, "<c-q>", mc.toggleCursor)
 
-        -- Mappings defined in a keymap layer only apply when there are
-        -- multiple cursors. This lets you have overlapping mappings.
-        mc.addKeymapLayer(function(layerSet)
+    -- Mappings defined in a keymap layer only apply when there are
+    -- multiple cursors. This lets you have overlapping mappings.
+    mc.addKeymapLayer(function(layerSet)
+      -- Select a different cursor as the main one.
+      layerSet({ "n", "x" }, "<C-A-left>", mc.prevCursor)
+      layerSet({ "n", "x" }, "<C-A-right>", mc.nextCursor)
 
-            -- Select a different cursor as the main one.
-            layerSet({"n", "x"}, "<C-A-left>", mc.prevCursor)
-            layerSet({"n", "x"}, "<C-A-right>", mc.nextCursor)
+      -- Delete the main cursor.
+      layerSet({ "n", "x" }, "<leader><C-x>", mc.deleteCursor)
 
-            -- Delete the main cursor.
-            layerSet({"n", "x"}, "<leader><C-x>", mc.deleteCursor)
+      -- Enable and clear cursors using escape.
+      layerSet("n", "<esc>", function()
+        if not mc.cursorsEnabled() then
+          mc.enableCursors()
+        else
+          mc.clearCursors()
+        end
+      end)
+    end)
 
-            -- Enable and clear cursors using escape.
-            layerSet("n", "<esc>", function()
-                if not mc.cursorsEnabled() then
-                    mc.enableCursors()
-                else
-                    mc.clearCursors()
-                end
-            end)
-        end)
-
-        -- Customize how cursors look.
-        local hl = vim.api.nvim_set_hl
-        hl(0, "MultiCursorCursor", { reverse = true })
-        hl(0, "MultiCursorVisual", { link = "Visual" })
-        hl(0, "MultiCursorSign", { link = "SignColumn"})
-        hl(0, "MultiCursorMatchPreview", { link = "Search" })
-        hl(0, "MultiCursorDisabledCursor", { reverse = true })
-        hl(0, "MultiCursorDisabledVisual", { link = "Visual" })
-        hl(0, "MultiCursorDisabledSign", { link = "SignColumn"})
-    end
+    -- Customize how cursors look.
+    local hl = vim.api.nvim_set_hl
+    hl(0, "MultiCursorCursor", { reverse = true })
+    hl(0, "MultiCursorVisual", { link = "Visual" })
+    hl(0, "MultiCursorSign", { link = "SignColumn" })
+    hl(0, "MultiCursorMatchPreview", { link = "Search" })
+    hl(0, "MultiCursorDisabledCursor", { reverse = true })
+    hl(0, "MultiCursorDisabledVisual", { link = "Visual" })
+    hl(0, "MultiCursorDisabledSign", { link = "SignColumn" })
+  end,
 }
