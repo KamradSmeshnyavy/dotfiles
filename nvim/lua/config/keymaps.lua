43db440530f9163c24a1926ed11ec7d2eaa026c3
KamradSmeshnyavy <ibragimovdenis36@gmail.com>
2026-01-28 04:25:44 +0300

    keymaps for lua

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -26,3 +26,25 @@ map("n", "p", '"+p', { noremap = true, silent = true })
 
 vim.api.nvim_set_keymap("i", "jj", "<Esc>", { noremap = false })
 vim.api.nvim_set_keymap("i", "оо", "<Esc>", { noremap = false })
+
+-- [ Группа управления файлами и интерфейсом ]
+map("n", "<leader><C-l>d", function()
+  Snacks.dashboard.open()
+end, { desc = "Открыть Dashboard" })
+map("n", "<leader><C-l>f", function()
+  vim.lsp.buf.format()
+end, { desc = "Форматировать код (LSP)" })
+
+-- [ Группа управления плагинами ]
+map("n", "<leader><C-l>l", ":Lazy<CR>", { desc = "Меню плагинов (Lazy)" })
+map("n", "<leader><C-l>m", ":Mason<CR>", { desc = "Менеджер серверов (Mason)" })
+
+vim.keymap.set("n", "<leader>bx", function()
+  local bufnr = 0
+  local is_enabled = vim.diagnostic.is_enabled({ bufnr = bufnr })
+  vim.diagnostic.enable(not is_enabled, { bufnr = bufnr })
+
+  -- Необязательно: уведомление о статусе
+  local status = is_enabled and "OFF" or "ON"
+  print("Diagnostics " .. status)
+end, { desc = "Toggle diagnostics in buffer" })
