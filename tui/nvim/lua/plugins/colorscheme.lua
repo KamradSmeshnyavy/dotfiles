5c260091730fad993e11073c9aa2ba482ffeb2aa
KamradSmeshnyavy <ibragimovdenis36@gmail.com>
2026-04-22 22:05:36 +0300

    testing transparent

---
+++ tui/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,39 @@
+return {}
+-- return {
+--
+--   {
+--     "catppuccin/nvim",
+--     name = "catppuccin",
+--     opts = {
+--       transparent_background = true,
+--       integrations = {
+--         alpha = true,
+--         cmp = true,
+--         gitsigns = true,
+--         illuminate = true,
+--         indent_blankline = { enabled = true },
+--         lsp_trouble = true,
+--         mason = true,
+--         mini = true,
+--         native_lsp = {
+--           enabled = true,
+--           underlines = {
+--             errors = { "undercurl" },
+--             hints = { "undercurl" },
+--             warnings = { "undercurl" },
+--             information = { "undercurl" },
+--           },
+--         },
+--         navic = { enabled = true, custom_bg = "NONE" },
+--         neotest = true,
+--         noice = true,
+--         notify = true,
+--         neotree = true,
+--         semantic_tokens = true,
+--         telescope = { enabled = true },
+--         treesitter = true,
+--         which_key = true,
+--       },
+--     },
+--   },
+-- }
