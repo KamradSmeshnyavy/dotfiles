e0376f959d16bc65c97ea11f4a2fcf763ff002e5
KamradSmeshnyavy <ibragimovdenis36@gmail.com>
2026-08-05 01:06:50 +0300

    firefox user config

---
+++ apps/chromium/configs/firefox/user.js
@@ -0,0 +1,6 @@
+// --- Added by Opencode for Wayland & Battery Optimization ---
+user_pref("widget.wayland.opaque-region.enabled", true);
+user_pref("gfx.webrender.compositor", true);
+// user_pref("gfx.webrender.compositor.force-enabled", true);
+user_pref("layout.frame_rate", 60);
+// ------------------------------------------------------------
